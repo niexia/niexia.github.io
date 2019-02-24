2f3ceb64740da14a478417b46f6a2ab207cde22d
yangjin <kingyouge@gmail.com>
2019-02-24 10:39:45 +0800

    modify(index.js): 修改接口请求

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var PostList = Vue.extend({
       var page = params.page || 1;
       var isHome = true;
       var label = '';
-      var cache = CACHE[label + 'Page' + page];
+      var cache = CACHE[label + '-' + page];
       if (params.name) {
         label = params.name;
         isHome = false;
@@ -33,21 +33,19 @@ var PostList = Vue.extend({
         this.label = cache.label;
         return;
       }
-      this.$http({
-        method: 'GET',
-        url:'https://api.github.com/repos/' + config['user'] + '/' + config['repo'] + '/issues',
-        data: {
-          creator: config['user'],
-          page: page,
-          labels: label
-        }
-      }).then(function(response) {
+      this.$http.get('https://api.github.com/repos/' + config['user'] + '/' + config['repo'] + '/issues',
+        {
+          params: {
+            creator: config['user'],
+            page: page,
+            labels: label
+          }
+        }).then(function(response) {
         console.log(response);
         var data = response.data;
-        var link = '';
-          // link = response.headers('Link'),
         var prePage = false;
         var nextPage = false;
+        var link = response.headers['Link'];
         if (link && link.indexOf('rel="prev"') > 0) prePage = parseInt(page) - 1;
         if (link && link.indexOf('rel="next"') > 0) nextPage = parseInt(page) + 1;
         this.posts = data;
@@ -60,20 +58,20 @@ var PostList = Vue.extend({
         if (page != 1) title = 'Page ' + page + config['sep'] + title;
         if (label) title = label + config['sep'] + title;
         document.title = title;
-        CACHE[label + 'Page' + page] = {
+        CACHE[label + '-' + page] = {
           posts: data,
           prePage: prePage,
           nextPage: nextPage,
           isHome: isHome,
           label: label
         };
-        for (var i in data) {
-          if (!CACHE.hasOwnProperty('Post' + data[i].number)) {
-            CACHE['Post' + data[i].number] = {
-              post: data[i]
-            };
+        data.forEach(function(post) {
+          if (!CACHE['post' + post.id]) {
+            CACHE['post'+ post.id] = {
+              post: post
+            }
           }
-        }
+        })
       }).catch(function(error) {
         throw error;
       })
@@ -109,21 +107,18 @@ var PostDetail = Vue.extend({
   methods: {
     handleGetPostListDetail: function () {
       var id = this.$route.params['id'];
-      var cache = CACHE['Post' + id];
+      var cache = CACHE['post' + id];
       if (cache) {
         var data = cache.post;
         data.body = marked(data.body);
         this.$data.post = data;
         return;
       }
-      this.$http({
-        method: 'GET',
-        url:'https://api.github.com/repos/' + config['user'] + '/' + config['repo'] + '/issues/' + id
-      }).then(function(response) {
+      this.$http.get('https://api.github.com/repos/' + config['user'] + '/' + config['repo'] + '/issues/' + id
+        ).then(function(response) {
         var data = response.data;
         data.body = marked(data.body);
         this.post = data;
-        console.log(data);
         document.title = data.title + config['sep'] + config['blogname'];
       }).catch(function (error) {
         throw error;
