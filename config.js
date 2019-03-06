e3a6c6d93b65dee0327d8757cb9465ce4d50dcdb
yangjin <kingyouge@gmail.com>
2019-03-06 15:18:13 +0800

    feat: add github-markdown.css, adjust page display

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  blogname: 'YangJin Blog', // 博客名称
-  sep: ' | ', // document.title 的分割
-  user: 'yangseas', // GitHub账号
-  repo: 'yangseas.github.io', // GitHub res名称
-  per_page: 15 // 每页多少篇博客
-};
