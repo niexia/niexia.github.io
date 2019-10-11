a0b687eadcfef9f41abd7e3099de1e6160a4119b
yangjin <kingyouge@gmail.com>
2019-10-11 10:05:40 +0800

    modify: 修改用户名

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
   blogname: 'YangJin Blog', // 博客名称
   sep: ' | ', // document.title 的分割
-  user: 'yangseas', // GitHub账号
-  repo: 'yangseas.github.io', // GitHub res名称
+  user: 'niexias', // GitHub账号
+  repo: 'niexias.github.io', // GitHub res名称
   per_page: 15 // 每页多少篇博客
 };
