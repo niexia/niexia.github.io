5bd00a384c5c6412bc080da7bd447117d6bd877d
yangjin <yangjin.im@bytedance.com>
2025-06-29 20:49:45 +0800

    feat: add sitemap generation functionality for improved SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const SITE_URL = 'https://yangjin-blog.vercel.app';
+
+async function getNoteSlugs(dir: string) {
+  const entries = await fs.readdir(dir, {
+    recursive: true,
+    withFileTypes: true
+  });
+  return entries
+    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
+    .map((entry) => {
+      const relativePath = path.relative(
+        dir,
+        path.join(entry.parentPath, entry.name)
+      );
+      return path.dirname(relativePath);
+    })
+    .map((slug) => slug.replace(/\\/g, '/'));
+}
+
+/** 
+ * generate sitemap.xml file
+ * sitemap.(xml|js|ts) is a special file that matches the Sitemaps XML format to help search engine crawlers index your site more efficiently.
+ */
+export default async function sitemap() {
+  const notesDirectory = path.join(process.cwd(), 'app', 'posts');
+  const slugs = await getNoteSlugs(notesDirectory);
+
+  const notes = slugs.map((slug) => ({
+    url: `${SITE_URL}/posts/${slug}`,
+    lastModified: new Date().toISOString()
+  }));
+
+  const routes = ['', '/posts', '/projects', '/links'].map((route) => ({
+    url: `${SITE_URL}${route}`,
+    lastModified: new Date().toISOString()
+  }));
+
+  return [...routes, ...notes];
+}
