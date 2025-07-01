fbe564d57b16e5e1dc119a8954d9cd6cd81ad936
yangjin <yangjin.im@bytedance.com>
2025-07-02 00:53:59 +0800

    fix: replace div elements with i tags for icon rendering in NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -24,21 +24,21 @@ const NavBar = () => {
         <div className="grid grid-cols-[repeat(7,auto)] gap-4">
           <Link href="/posts" title="Blog">
             <span className="hidden md:inline">Blog</span>
-            <div className="ri-article-fill md:hidden"></div>
+            <i className="ri-article-fill md:hidden"></i>
           </Link>
           <Link href="/projects" title="Projects">
             <span className="hidden md:inline">Projects</span>
-            <div className="ri-lightbulb-fill md:hidden"></div>
+            <i className="ri-lightbulb-fill md:hidden"></i>
           </Link>
           <Link href="/Links" title="Links">
             <span className="hidden md:inline">Links</span>
-            <div className="ri-links-fill md:hidden"></div>
+            <i className="ri-links-fill md:hidden"></i>
           </Link>
           <a href="https://x.com/yangjinfe" target="_blank" title="X">
-            <div className="ri-twitter-fill" />
+            <i className="ri-twitter-fill" />
           </a>
           <a href="https://github.com/niexia" target="_blank" title="Bluesky">
-            <div className="ri-github-fill" />
+            <i className="ri-github-fill" />
           </a>
           {/* <a href="/feed.xml" target="_blank" title="RSS">
             <div className="font-size:1.25rem; margin: 0 -0.125rem;" />
